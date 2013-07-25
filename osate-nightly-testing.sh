958fae9380a794682429228d7a7ee449d14e5022
Julien Delange <julien@gunnm.org>
2013-07-25 09:02:43 -0400

    New target for kepler

--- osate-nightly-testing.sh
+++ osate-nightly-testing.sh
@@ -2,23 +2,24 @@
 
 BUILDDIR=/tmp/osate
 
-VERSION=2.0.2-snapshot
+VERSION=2.0.4-snapshot
 DATE=`date '+%Y%m%d'`
-JUNO_PLATFORM_URL="http://ftp.osuosl.org/pub/eclipse//eclipse/downloads/drops4/R-4.2.2-201302041200/org.eclipse.platform-4.2.2.zip"
-JUNO_PLATFORM_FILE="org.eclipse.platform-4.2.2.zip"
+
+KEPLER_PLATFORM_URL="http://ftp.ussg.iu.edu/eclipse/eclipse/downloads/drops4/R-4.3-201306052000/org.eclipse.platform-4.3.zip"
+KEPLER_PLATFORM_FILE="org.eclipse.platform-4.3.zip"
 
 mkdir -p ${BUILDDIR}
 
 
-if [ ! -f $JUNO_PLATFORM_FILE ]; then
-	wget -c $JUNO_PLATFORM_URL
+if [ ! -f $KEPLER_PLATFORM_FILE ]; then
+	wget -c $KEPLER_PLATFORM_URL
 fi
 (cd ${BUILDDIR} && rm -rf core)
 (cd ${BUILDDIR} && rm -rf plugins)
 (cd ${BUILDDIR} && rm -rf error-model1)
 (cd ${BUILDDIR} && rm -rf error-model2)
-(cp -f $JUNO_PLATFORM_FILE /tmp)
-(cd /tmp && unzip -f $JUNO_PLATFORM_FILE )
+(cp -f $KEPLER_PLATFORM_FILE /tmp)
+(cd /tmp && unzip -f $KEPLER_PLATFORM_FILE )
 (cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate2-core.git core)
 (cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate2-plugins.git plugins)
 (cd ${BUILDDIR} && git clone -b develop https://github.com/osate/ErrorModelV1.git error-model1)
@@ -30,7 +31,6 @@ fi
 (cd ${BUILDDIR} && sed -i "s/RELEASE_DATE/$DATE/g" "core/org.osate.branding/plugin.properties" )
 (cd ${BUILDDIR} && sed -i "s/RELEASE_DATE/$DATE/g" "core/org.osate.branding/about.mappings" )
 
-(cp -f indigo.target ${BUILDDIR}/core/org.osate.build.target/indigo.target )
-(cp -f juno.target ${BUILDDIR}/core/org.osate.build.target/juno.target )
+(cp -f kepler.target ${BUILDDIR}/core/org.osate.build.target/kepler.target )
 (cd ${BUILDDIR}/core/org.osate.build.main && mvn clean install) || exit 1
 exit 0
