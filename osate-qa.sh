438bdeabe622e081a8546fbd1e0160f9171dd711
Julien <julien@gunnm.org>
2014-02-05 16:11:33 -0500

    start to make it working

--- osate-qa.sh
+++ osate-qa.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-RAMSES_URL=http://www.aadl.info/aadl/osate/misc/ramses/
 rm -rf /tmp/osate-qa
-cd (/tmp/osate-qa && git checkout https://github.com/khoroshilov/aadl-qa.git aadl-qa)
+mkdir -p /tmp/osate-qa
+(cd /tmp/osate-qa && git clone https://github.com/khoroshilov/aadl-qa.git aadl-qa)
 
-if [ ! -f /tmp/osate-qa/ramses.zip ]; then
-	(cd /tmp/osate-qa && wget -c $RAMSES_URL)
+if [ -f /tmp/osate-ramses.zip ]; then
+	(cp /tmp/osate-ramses.zip /tmp/osate-qa/ramses.zip)
+else
+	exit 3
 fi
 
 (cd /tmp/osate-qa && unzip ramses.zip )
-(cd /tmp/osate-qa/aadl-qa && export PATH=/tmp/osate-qa/ramses/:$PATH ./aadl=qa.pl run Osate )
+(cd /tmp/osate-qa/aadl-qa && export PATH=/tmp/osate-qa/ramses/:$PATH && ./aadl-qa.pl run Osate )
 
