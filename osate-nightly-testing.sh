7f585dc12bb57a07dea1115bb4cd9870dfb9f72f
Julien Delange <julien@gunnm.org>
2014-07-16 12:44:06 -0400

    start to integrate luna

--- osate-nightly-testing.sh
+++ osate-nightly-testing.sh
@@ -2,17 +2,16 @@
 
 BUILDDIR=/tmp/osate
 
-VERSION=2.0.6-snapshot
+VERSION=2.0.8-snapshot
 DATE=`date '+%Y%m%d'`
 
-KEPLER_PLATFORM_URL="http://ftp.ussg.iu.edu/eclipse/eclipse/downloads/drops4/R-4.3-201306052000/org.eclipse.platform-4.3.zip"
-KEPLER_PLATFORM_FILE="org.eclipse.platform-4.3.zip"
-
+PLATFORM_URL="http://ftp.ussg.iu.edu/eclipse/eclipse/downloads/drops4/R-4.4-201406061215/org.eclipse.platform-4.4.zip"
+PLATFORM_FILE="org.eclipse.platform-4.4.zip"
 mkdir -p ${BUILDDIR}
 
 
-if [ ! -f $KEPLER_PLATFORM_FILE ]; then
-	wget -c $KEPLER_PLATFORM_URL
+if [ ! -f $PLATFORM_FILE ]; then
+	wget -c $PLATFORM_URL
 fi
 (cd ${BUILDDIR} && rm -rf core)
 (cd ${BUILDDIR} && rm -rf plugins)
@@ -21,8 +20,8 @@ fi
 (cd ${BUILDDIR} && rm -rf osate-ge)
 (cd ${BUILDDIR} && rm -rf osate2-ba)
 
-(cp -f $KEPLER_PLATFORM_FILE /tmp)
-(cd /tmp && unzip -f $KEPLER_PLATFORM_FILE )
+(cp -f $PLATFORM_FILE /tmp)
+(cd /tmp && unzip -f $PLATFORM_FILE )
 (cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate2-core.git core)
 (cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate2-plugins.git plugins)
 (cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate-ge.git osate-ge)
@@ -37,6 +36,6 @@ fi
 (cd ${BUILDDIR} && sed -i "s/RELEASE_DATE/$DATE/g" "core/org.osate.branding/plugin.properties" )
 (cd ${BUILDDIR} && sed -i "s/RELEASE_DATE/$DATE/g" "core/org.osate.branding/about.mappings" )
 
-(cp -f kepler.target ${BUILDDIR}/core/org.osate.build.target/kepler.target )
+(cp -f luna.target ${BUILDDIR}/core/org.osate.build.target/luna.target )
 (cd ${BUILDDIR}/core/org.osate.build.main && mvn clean install) || exit 1
 exit 0
