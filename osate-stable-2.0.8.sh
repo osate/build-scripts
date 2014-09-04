9158ab320a6ad041c47114427ecb19ea2b8ed01d
Julien <julien@gunnm.org>
2014-09-04 13:42:04 -0400

    Add script for new stable

---
+++ osate-stable-2.0.8.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+BUILDDIR=/tmp/osate
+
+VERSION=2.0.8
+DATE=`date '+%Y%m%d'`
+
+PLATFORM_URL="http://ftp.ussg.iu.edu/eclipse/eclipse/downloads/drops4/R-4.4-201406061215/org.eclipse.platform-4.4.zip"
+PLATFORM_FILE="org.eclipse.platform-4.4.zip"
+
+mkdir -p ${BUILDDIR}
+
+
+if [ ! -f $PLATFORM_FILE ]; then
+	wget -c $PLATFORM_URL
+fi
+(cd ${BUILDDIR} && rm -rf core)
+(cd ${BUILDDIR} && rm -rf plugins)
+(cd ${BUILDDIR} && rm -rf osate-ge)
+(cd ${BUILDDIR} && rm -rf error-model1)
+(cd ${BUILDDIR} && rm -rf error-model2)
+(cp -f $PLATFORM_FILE /tmp)
+(cd /tmp && unzip -f $PLATFORM_FILE )
+(cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate2-core.git core)
+(cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate2-plugins.git plugins)
+(cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate-ge.git osate-ge)
+(cd ${BUILDDIR} && git clone -b develop https://github.com/osate/ErrorModelV1.git error-model1)
+(cd ${BUILDDIR} && git clone -b develop https://github.com/osate/ErrorModelV2.git error-model2)
+
+(cd ${BUILDDIR} && git clone -b develop https://github.com/osate/osate2-ba.git aadlba)
+
+(cd ${BUILDDIR} && sed -i "s/RELEASE_VERSION/$VERSION/g" "core/org.osate.branding/plugin.properties" )
+(cd ${BUILDDIR} && sed -i "s/RELEASE_VERSION/$VERSION/g" "core/org.osate.branding/about.mappings" )
+
+(cd ${BUILDDIR} && sed -i "s/RELEASE_DATE/$DATE/g" "core/org.osate.branding/plugin.properties" )
+(cd ${BUILDDIR} && sed -i "s/RELEASE_DATE/$DATE/g" "core/org.osate.branding/about.mappings" )
+
+(cp -f kepler.target ${BUILDDIR}/core/org.osate.build.target/luna.target )
+(cd ${BUILDDIR}/core/org.osate.build.main && mvn clean install) || exit 1
+exit 0
